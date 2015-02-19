603fe52f29981ac96c2f397765e0b500ba73640d
Albéric <alberic.trancart@eleves.enpc.fr>
2015-02-19 15:57:35 +0100

    Les nouveaux arrivants peuvent maintenant se logger avec leurs identifiants DSI

--- back/src/KI/UpontBundle/Services/KICurl.php
+++ back/src/KI/UpontBundle/Services/KICurl.php
@@ -11,7 +11,7 @@ class KICurl extends ContainerAware
     {
         $proxyUrl = $this->container->getParameter('proxy_url');
         $proxyUser = $this->container->getParameter('proxy_user');
-        
+
         // Réglage des options cURL
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -21,19 +21,19 @@ class KICurl extends ContainerAware
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_HTTP_VERSION, 'CURL_HTTP_VERSION_1_0');
         curl_setopt($ch, CURLOPT_USERAGENT, 'runscope/0.1');
-        
+
         // curl_setopt($ch, CURLOPT_VERBOSE, true);
         // curl_setopt($ch, CURLOPT_STDERR, fopen('/var/www/youpont/v2/app/logs/curl.log', 'a'));
-        
-        foreach($options as $option => $value) {
-            curl_setopt($ch, $option, $value);
-        }
 
         if ($proxyUrl !== null)
             curl_setopt($ch, CURLOPT_PROXY, $proxyUrl);
         if ($proxyUser !== null)
             curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyUser);
-        
+
+        foreach($options as $option => $value) {
+            curl_setopt($ch, $option, $value);
+        }
+
         // Récupération de la ressource
         $data = curl_exec($ch);
         curl_close($ch);
