db361f487e9bc05bd1e87a763134f1fb77252142
Albéric <alberic.trancart@eleves.enpc.fr>
2015-02-19 10:18:19 +0100

    Fin systeme de commentaires

--- back/src/KI/UpontBundle/Tests/Controller/Ponthub/MoviesControllerTest.php
+++ back/src/KI/UpontBundle/Tests/Controller/Ponthub/MoviesControllerTest.php
@@ -8,45 +8,45 @@ class MoviesControllerTest extends WebTestCase
 {
     public function testGetAll()
     {
-        $this->client->request('GET', '/ponthub/movies');
+        $this->client->request('GET', '/movies');
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 200);
 
-        $this->client->request('GET', '/ponthub/movies/pumping-iron');
+        $this->client->request('GET', '/movies/pumping-iron');
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 200);
 
-        $this->client->request('GET', '/ponthub/movies/sjoajsiohaysahais-asbsksaba7');
+        $this->client->request('GET', '/movies/sjoajsiohaysahais-asbsksaba7');
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 404);
     }
 
     public function testPatch()
     {
-        $this->client->request('PATCH', '/ponthub/movies/pumping-iron', array('actors' => array(array('name' => 'Arnold Schwarzenegger')), 'genres' => array(array('name' => 'Bodybuilding'), array('name' => 'Documentaire')), 'year' => 1977, 'tags' => array(array('name' => 'hjihjk'))));
+        $this->client->request('PATCH', '/movies/pumping-iron', array('actors' => array(array('name' => 'Arnold Schwarzenegger')), 'genres' => array(array('name' => 'Bodybuilding'), array('name' => 'Documentaire')), 'year' => 1977, 'tags' => array(array('name' => 'hjihjk'))));
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 204);
 
-        $this->client->request('PATCH', '/ponthub/movies/pumping-iron', array('size' => 0));
+        $this->client->request('PATCH', '/movies/pumping-iron', array('size' => 0));
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 400);
 
-        $this->client->request('PATCH', '/ponthub/movies/sjoajsiohaysahais-asbsksaba7', array('username' => 'miam', 'email' => '123@mail.fr'));
+        $this->client->request('PATCH', '/movies/sjoajsiohaysahais-asbsksaba7', array('username' => 'miam', 'email' => '123@mail.fr'));
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 404);
     }
 
     public function testStats()
     {
-        $this->client->request('GET', '/ponthub/movies/pumping-iron/download');
+        $this->client->request('GET', '/movies/pumping-iron/download');
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 302);
 
-        $this->client->request('GET', '/ponthub/movies/pumping-iron/download');
+        $this->client->request('GET', '/movies/pumping-iron/download');
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 302);
 
-        $this->client->request('GET', '/ponthub/movies/pumping-iron');
+        $this->client->request('GET', '/movies/pumping-iron');
         $response = $this->client->getResponse();
         $infos = json_decode($response->getContent(), true);
         $this->assertJsonResponse($response, 200);
--- back/src/KI/UpontBundle/Tests/Controller/Ponthub/RequestsControllerTest.php
+++ back/src/KI/UpontBundle/Tests/Controller/Ponthub/RequestsControllerTest.php
@@ -9,73 +9,73 @@ class RequestsControllerTest extends WebTestCase
     // On crée une ressource sur laquelle seront effectués les tests. Ne pas oublier de supprimer à la fin avec le test DELETE.
     public function testPost()
     {
-        $this->client->request('POST', '/ponthub/requests', array('name' => 'Sucker Punch'));
+        $this->client->request('POST', '/requests', array('name' => 'Sucker Punch'));
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 201);
         // On vérifie que le lieu du nouvel objet a été indiqué
         $this->assertTrue($response->headers->has('Location'), $response->headers);
     }
-   
+
     public function testGet()
     {
-        $this->client->request('GET', '/ponthub/requests');
+        $this->client->request('GET', '/requests');
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 200);
 
-        $this->client->request('GET', '/ponthub/requests/sucker-punch');
+        $this->client->request('GET', '/requests/sucker-punch');
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 200);
 
-        $this->client->request('GET', '/ponthub/requests/sjoajsiohaysahais-asbsksaba7');
+        $this->client->request('GET', '/requests/sjoajsiohaysahais-asbsksaba7');
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 404);
     }
-    
+
     public function testUpvote()
     {
-        $this->client->request('PATCH', '/ponthub/requests/sucker-punch/upvote');
+        $this->client->request('PATCH', '/requests/sucker-punch/upvote');
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 204);
-        
-        $this->client->request('PATCH', '/ponthub/requests/sucker-punch/upvote');
+
+        $this->client->request('PATCH', '/requests/sucker-punch/upvote');
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 204);
-        
-        $this->client->request('PATCH', '/ponthub/requests/sucker-punch/upvote');
+
+        $this->client->request('PATCH', '/requests/sucker-punch/upvote');
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 204);
-        
-        $this->client->request('PATCH', '/ponthub/requests/susqdsqdsq/upvote');
+
+        $this->client->request('PATCH', '/requests/susqdsqdsq/upvote');
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 404);
     }
-    
+
     public function testDownvote()
     {
-        $this->client->request('PATCH', '/ponthub/requests/sucker-punch/downvote');
+        $this->client->request('PATCH', '/requests/sucker-punch/downvote');
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 204);
-        
-        $this->client->request('PATCH', '/ponthub/requests/suckeqsdqdsqqr-punch/downvote');
+
+        $this->client->request('PATCH', '/requests/suckeqsdqdsqqr-punch/downvote');
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 404);
-        
+
         // Le compte devrait maintenant être à 3 votes
-        $this->client->request('GET', '/ponthub/requests/sucker-punch');
+        $this->client->request('GET', '/requests/sucker-punch');
         $response = $this->client->getResponse();
         $infos = json_decode($response->getContent(), true);
         $this->assertJsonResponse($response, 200);
         $this->assertNotEquals($infos, null);
         $this->assertEquals($infos['votes'], 3);
     }
-    
+
     public function testDelete()
     {
-        $this->client->request('DELETE', '/ponthub/requests/sucker-punch');
+        $this->client->request('DELETE', '/requests/sucker-punch');
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 204);
-        
-        $this->client->request('DELETE', '/ponthub/requests/sucker-punch');
+
+        $this->client->request('DELETE', '/requests/sucker-punch');
         $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 404);
     }
