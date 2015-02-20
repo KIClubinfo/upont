817942ccb2b0fe68bc3cb005ba34bb24a337d836
Albéric <alberic.trancart@eleves.enpc.fr>
2015-02-20 10:22:59 +0100

    Pas besoin de config proxy Git pour les Linuxiens

--- back/src/KI/UpontBundle/Tests/Services/KICurlTest.php
+++ back/src/KI/UpontBundle/Tests/Services/KICurlTest.php
@@ -9,17 +9,18 @@ class KICurlTest extends WebTestCase
 {
     protected $container;
     protected $service;
-    
+
     public function __construct()
     {
         parent::__construct();
         $this->container = static::$kernel->getContainer();
         $this->service = $this->container->get('ki_upont.curl');
     }
-    
+
     public function testCurl()
     {
         $response = $this->service->curl('https://www.google.fr');
+        $this->assertInternalType('string', $response);
         $this->assertContains('<!doctype html>', $response);
     }
 }
