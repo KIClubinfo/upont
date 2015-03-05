feb6ef388eeb3d630412c4bf076be138ef8aaa36
Albéric <alberic.trancart@eleves.enpc.fr>
2015-03-05 21:45:07 +0100

    Mise en valeur des shotguns

--- mobile/app/js/app.js
+++ mobile/app/js/app.js
@@ -1,5 +1,5 @@
-var url = 'https://upont.enpc.fr/api';
-//var url = 'http://localhost/api';
+//var url = 'https://upont.enpc.fr/api';
+var url = 'http://localhost/api';
 moment.locale('fr');
 
 var onsAlert = function(title, message) {
