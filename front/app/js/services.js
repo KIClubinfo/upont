c54baa4f43d0bae8fd146ca62cbc7a1b966c316f
Albéric <alberic.trancart@eleves.enpc.fr>
2015-03-07 12:02:20 +0100

    Ajout Moment.js

--- front/app/js/services.js
+++ front/app/js/services.js
@@ -86,4 +86,10 @@ angular.module('upont')
             }
             return '#';
         };
+    })
+    .filter('formatDate', function() {
+        return function(date) {
+            date = moment.unix(date);
+            return ucfirst(date.calendar());
+        };
     });
