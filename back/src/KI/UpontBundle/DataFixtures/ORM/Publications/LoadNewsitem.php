a47a8926707351dfc9672928e6fe0ed14b616c96
Albéric <alberic.trancart@eleves.enpc.fr>
2015-03-07 17:17:24 +0100

    Ajout retours à la ligne dans les Fixtures

--- back/src/KI/UpontBundle/DataFixtures/ORM/Publications/LoadNewsitem.php
+++ back/src/KI/UpontBundle/DataFixtures/ORM/Publications/LoadNewsitem.php
@@ -13,10 +13,10 @@ class LoadNewsitemFixture extends AbstractFixture implements OrderedFixtureInter
     {
         $newsitem = new Newsitem();
         $newsitem->setName('Le Jeu');
-        $newsitem->setTextLong('Vous avez perdu au jeu, c\'est bien dommage.
-Il existe trois règles fondamentales :
-- Tout le monde joue au Jeu (parfois restreint en : "tous les gens qui connaissent le Jeu y jouent", ou alors en "Tu joues continuellement au Jeu").
-- Qui pense au jeu y perd immédiatement. Si vous lisez ceci, vous avez donc perdu.
+        $newsitem->setTextLong('Vous avez perdu au jeu, c\'est bien dommage.<br>
+Il existe trois règles fondamentales :<br>
+- Tout le monde joue au Jeu (parfois restreint en : "tous les gens qui connaissent le Jeu y jouent", ou alors en "Tu joues continuellement au Jeu").<br>
+- Qui pense au jeu y perd immédiatement. Si vous lisez ceci, vous avez donc perdu.<br>
 - Chaque défaite doit être annoncée à au moins une personne. Une phrase comme "J\'ai perdu", "J\'ai perdu le jeu", "Le jeu" ou chez quelques joueurs "Ca sent la perte" est ici très souvent employée. De plus, si vous faites perdre quelqu\'un, vous devez aussi l\'annoncer par un "Tu as perdu".');
         $newsitem->setDate(1414242424);
         $newsitem->setAuthorClub($this->getReference('club-ki'));
@@ -56,21 +56,21 @@ Il existe trois règles fondamentales :
 
         $newsitem = new Newsitem();
         $newsitem->setName('PEP t\'accompagne dans ton projet');
-        $newsitem->setTextLong('Cher futur entrepreneur,
-
-En tant que Junior-Entreprise, Ponts Etudes Projets a la possibilité de booster ton projet de création d\'entreprise grâce à Alten, cabinet de conseil en ingénierie et innovation dont nous sommes partenaires, et ce dans le cadre d\'un concours national.
-
-À la clef :
-- l\'appui d\'un grand cabinet de conseil en ingénierie pour monter ta boite ;
-- l\'hébergement du projet, dont 5 à 10 PFE conduits au sein de la Direction R&D d\'Alten ;
-- des formations pour ces stagiaires au management de l\'innovation ;
-- et évidemment, TA boîte.
-
-Pour cela, ton projet doit :
-- Etre innovant, technologique et moderne ;
-- Avoir un potentiel impact sociétal important ;
-- Etre réalisable à court terme (6 mois);
-
+        $newsitem->setTextLong('Cher futur entrepreneur,<br>
+<br>
+En tant que Junior-Entreprise, Ponts Etudes Projets a la possibilité de booster ton projet de création d\'entreprise grâce à Alten, cabinet de conseil en ingénierie et innovation dont nous sommes partenaires, et ce dans le cadre d\'un concours national.<br>
+<br>
+À la clef :<br>
+- l\'appui d\'un grand cabinet de conseil en ingénierie pour monter ta boite ;<br>
+- l\'hébergement du projet, dont 5 à 10 PFE conduits au sein de la Direction R&D d\'Alten ;<br>
+- des formations pour ces stagiaires au management de l\'innovation ;<br>
+- et évidemment, TA boîte.<br>
+<br>
+Pour cela, ton projet doit :<br>
+- Etre innovant, technologique et moderne ;<br>
+- Avoir un potentiel impact sociétal important ;<br>
+- Etre réalisable à court terme (6 mois);<br>
+<br>
 Pour plus de détails sur le concours, contacte David (david.dessalles@eleves.enpc.fr). Saches que ton projet, s\'il est lauréat, deviendra NOTRE priorité stratégique de l\'année. Nous t\'attendons avec impatience pour préparer ensemble ta candidature.');
         $newsitem->setDate(1414462150);
         $newsitem->setAuthorClub($this->getReference('club-pep'));
