0383b6e97ecd80649e0eb858021015d31d4bce08
Albéric <alberic.trancart@eleves.enpc.fr>
2015-03-15 17:49:09 +0100

    Ajout du lien de synchronisation ICS dans l'appli mobile

--- mobile/app/js/controllers/news.js
+++ mobile/app/js/controllers/news.js
@@ -1,73 +1,73 @@
 module
     .controller('NewsController', ['$scope', 'StorageService', '$http', function($scope, StorageService, $http) {
-	    $scope.news = [];
-	    $scope.newItem = [];
-	    $scope.comments = [];
-	    $scope.url = url;
+        $scope.news = [];
+        $scope.newItem = [];
+        $scope.comments = [];
+        $scope.url = url;
 
-	    $scope.init = function($done){
-		    $http.get(url + '/own/newsitems?limit=20').success(function(data){
-			    $scope.news = data;
-		    })
-	        .finally(function() {
+        $scope.init = function($done){
+            $http.get(url + '/own/newsitems?limit=20').success(function(data){
+                $scope.news = data;
+            })
+            .finally(function() {
                 if ($done) {
                     $done();
                 }
             });
-	    };
+        };
 
-	    $scope.load = function(slug){
-		    $http.get(url + '/newsitems/' + slug).success(function(data){
-			    $scope.newItem = data;
-			    nav.pushPage('new.html');
-		    });
-	    };
+        $scope.load = function(slug){
+            $http.get(url + '/newsitems/' + slug).success(function(data){
+                $scope.newItem = data;
+                nav.pushPage('new.html');
+            });
+        };
 
-	    $scope.likeClick = function(){
-	        // Si la personne like déjà on ne fait qu'annuler le like
-	        if ($scope.newItem.like) {
-		        $http.delete(url + '/newsitems/' + $scope.newItem.slug + '/like').success(function(data){
-			        $scope.newItem.like = false;
-			        $scope.newItem.likes--;
-		        });
-		    } else {
-		        $http.post(url + '/newsitems/' + $scope.newItem.slug + '/like').success(function(data){
-			        $scope.newItem.like = true;
-			        $scope.newItem.likes++;
+        $scope.likeClick = function(){
+            // Si la personne like déjà on ne fait qu'annuler le like
+            if ($scope.newItem.like) {
+                $http.delete(url + '/newsitems/' + $scope.newItem.slug + '/like').success(function(data){
+                    $scope.newItem.like = false;
+                    $scope.newItem.likes--;
+                });
+            } else {
+                $http.post(url + '/newsitems/' + $scope.newItem.slug + '/like').success(function(data){
+                    $scope.newItem.like = true;
+                    $scope.newItem.likes++;
 
-			        // Si la personne unlikait avant
-			        if ($scope.newItem.dislike) {
-			            $scope.newItem.dislike = false;
-			            $scope.newItem.dislikes--;
-			        }
-		        });
-		    }
-	    };
+                    // Si la personne unlikait avant
+                    if ($scope.newItem.dislike) {
+                        $scope.newItem.dislike = false;
+                        $scope.newItem.dislikes--;
+                    }
+                });
+            }
+        };
 
-	    $scope.dislikeClick = function(){
-	        // Si la personne like déjà on ne fait qu'annuler le like
-	        if ($scope.newItem.dislike) {
-		        $http.delete(url + '/newsitems/' + $scope.newItem.slug + '/dislike').success(function(data){
-			        $scope.newItem.dislike = false;
-			        $scope.newItem.dislikes--;
-		        });
-		    } else {
-		        $http.post(url + '/newsitems/' + $scope.newItem.slug + '/dislike').success(function(data){
-			        $scope.newItem.dislike = true;
-			        $scope.newItem.dislikes++;
+        $scope.dislikeClick = function(){
+            // Si la personne like déjà on ne fait qu'annuler le like
+            if ($scope.newItem.dislike) {
+                $http.delete(url + '/newsitems/' + $scope.newItem.slug + '/dislike').success(function(data){
+                    $scope.newItem.dislike = false;
+                    $scope.newItem.dislikes--;
+                });
+            } else {
+                $http.post(url + '/newsitems/' + $scope.newItem.slug + '/dislike').success(function(data){
+                    $scope.newItem.dislike = true;
+                    $scope.newItem.dislikes++;
 
-			        // Si la personne unlikait avant
-			        if ($scope.newItem.like) {
-			            $scope.newItem.like = false;
-			            $scope.newItem.likes--;
-			        }
-		        });
-		    }
-	    };
+                    // Si la personne unlikait avant
+                    if ($scope.newItem.like) {
+                        $scope.newItem.like = false;
+                        $scope.newItem.likes--;
+                    }
+                });
+            }
+        };
 
-	    $scope.loadComments = function(){
-		    $http.get(url + '/newsitems/' + $scope.newItem.slug + '/comments').success(function(data){
-			    nav.pushPage('views/comments.html', {comments: data, route: '/newsitems/' + $scope.newItem.slug});
-		    });
-	    };
+        $scope.loadComments = function(){
+            $http.get(url + '/newsitems/' + $scope.newItem.slug + '/comments').success(function(data){
+                nav.pushPage('views/comments.html', {comments: data, route: '/newsitems/' + $scope.newItem.slug});
+            });
+        };
     }]);
--- mobile/app/js/controllers/settings.js
+++ mobile/app/js/controllers/settings.js
@@ -1,21 +1,22 @@
 module
     .controller('SettingsController', ['$scope', '$rootScope', 'StorageService', '$http', 'PushNotifications', function($scope, $rootScope, StorageService, $http, PushNotifications) {
-	    $scope.logout = function(){
-	        StorageService.remove('token');
-			StorageService.remove('token_exp');
-			onsAlert('Déconnexion', 'Tu as été correctement déconnecté');
-	        menu.setSwipeable(false);
+        $scope.logout = function(){
+            StorageService.remove('token');
+            StorageService.remove('token_exp');
+            onsAlert('Déconnexion', 'Tu as été correctement déconnecté');
+            menu.setSwipeable(false);
             menu.setMainPage('views/login.html', {closeMenu: true});
-	    };
+        };
 
-	    $scope.clubs = [];
-	    $scope.clubsNames = [];
-	    $scope.clubsFollowed = [];
-	    $scope.balance = null;
-	    $scope.pushable = false;
-	    $scope.notifs = [];
-	    $scope.notifTexts = {
-	        'notif_followed_event' : 'Nouvel événement',
+        $scope.clubs = [];
+        $scope.clubsNames = [];
+        $scope.clubsFollowed = [];
+        $scope.balance = null;
+        $scope.token = null;
+        $scope.pushable = false;
+        $scope.notifs = [];
+        $scope.notifTexts = {
+            'notif_followed_event' : 'Nouvel événement',
             'notif_followed_news'  : 'Nouvelle news',
             'notif_followed_poll'  : 'Nouveau sondage',
             'notif_ponthub'        : 'Nouveaux fichiers Ponthub',
@@ -26,74 +27,77 @@ module
             'notif_next_class'     : 'Un cours va bientôt commencer',
             'notif_achievement'    : 'Obtention d\'un achievement',
             'notif_next_level'     : 'Passage au niveau suivant'
-	    };
+        };
 
-	    $scope.init = function() {
-		    $http.get(url + '/foyer/balance').success(function(data){
-			    $scope.balance = data.balance;
-		    });
-		    $scope.pushable = /Android/i.test(navigator.userAgent);
-	    };
+        $scope.init = function() {
+            $http.get(url + '/foyer/balance').success(function(data){
+                $scope.balance = data.balance;
+            });
+            $http.get(url + '/own/token').success(function(data){
+                $scope.token = data.token;
+            });
+            $scope.pushable = /Android/i.test(navigator.userAgent);
+        };
 
-	    $scope.loadClubsFollowed = function() {
-	        $http.get(url + '/own/followed').success(function(data){
-	            for(var key in data) {
-	                if (data.hasOwnProperty(key)) {
-			            $scope.clubsFollowed.push(data[key].slug);
-			        }
-			    }
-		    });
-		    $http.get(url + '/clubs?sort=name').success(function(data){
-		        for(var key in data) {
-		            if (data.hasOwnProperty(key)) {
-			            $scope.clubs.push(data[key].slug);
-			            $scope.clubsNames.push(data[key].name);
-			        }
-			    }
-	        });
+        $scope.loadClubsFollowed = function() {
+            $http.get(url + '/own/followed').success(function(data){
+                for(var key in data) {
+                    if (data.hasOwnProperty(key)) {
+                        $scope.clubsFollowed.push(data[key].slug);
+                    }
+                }
+            });
+            $http.get(url + '/clubs?sort=name').success(function(data){
+                for(var key in data) {
+                    if (data.hasOwnProperty(key)) {
+                        $scope.clubs.push(data[key].slug);
+                        $scope.clubsNames.push(data[key].name);
+                    }
+                }
+            });
 
-		    nav.pushPage('clubsFollowed.html');
-	    };
+            nav.pushPage('clubsFollowed.html');
+        };
 
-	    $scope.changeFollowed = function(slug) {
-	        if ($scope.clubsFollowed.indexOf(slug) > -1) {
-		        $http.post(url + '/clubs/' + slug + '/unfollow').success(function(){
-			        $scope.clubsFollowed.splice($scope.clubsFollowed.indexOf(slug), 1);
-		        });
-		    } else {
-		        $http.post(url + '/clubs/' + slug + '/follow').success(function(){
-			        $scope.clubsFollowed.push(slug);
-		        });
-		    }
-	    };
-
-	    $scope.register = function() {
-	        PushNotifications.initialize();
-	    };
-
-	    $scope.unregister = function() {
-	        PushNotifications.unregister();
-	    };
-
-	    $scope.switchTheme = function() {
-	        $rootScope.dark = !$rootScope.dark;
-	        if ($rootScope.dark) {
-	            StorageService.set('dark', true);
-	        } else {
-	            StorageService.remove('dark');
+        $scope.changeFollowed = function(slug) {
+            if ($scope.clubsFollowed.indexOf(slug) > -1) {
+                $http.post(url + '/clubs/' + slug + '/unfollow').success(function(){
+                    $scope.clubsFollowed.splice($scope.clubsFollowed.indexOf(slug), 1);
+                });
+            } else {
+                $http.post(url + '/clubs/' + slug + '/follow').success(function(){
+                    $scope.clubsFollowed.push(slug);
+                });
             }
         };
 
-	    $scope.configNotifs = function() {
-		    $http.get(url + '/own/preferences').success(function(data){
-			    $scope.notifs = data;
-			    nav.pushPage('notifications.html');
-		    });
-	    };
+        $scope.register = function() {
+            PushNotifications.initialize();
+        };
 
-	    $scope.changeNotif = function(notif) {
-		    $http.patch(url + '/own/preferences', {key: notif, value: !$scope.notifs[notif]}).success(function(data){
-			    $scope.notifs[notif] = !$scope.notifs[notif];
-		    });
-	    };
+        $scope.unregister = function() {
+            PushNotifications.unregister();
+        };
+
+        $scope.switchTheme = function() {
+            $rootScope.dark = !$rootScope.dark;
+            if ($rootScope.dark) {
+                StorageService.set('dark', true);
+            } else {
+                StorageService.remove('dark');
+            }
+        };
+
+        $scope.configNotifs = function() {
+            $http.get(url + '/own/preferences').success(function(data){
+                $scope.notifs = data;
+                nav.pushPage('notifications.html');
+            });
+        };
+
+        $scope.changeNotif = function(notif) {
+            $http.patch(url + '/own/preferences', {key: notif, value: !$scope.notifs[notif]}).success(function(data){
+                $scope.notifs[notif] = !$scope.notifs[notif];
+            });
+        };
     }]);
