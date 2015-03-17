eba6c01cfffa2fb7071a8983d1875391a433d863
Albéric <alberic.trancart@eleves.enpc.fr>
2015-03-17 11:41:17 +0100

    Finition système de shotgun

--- front/app/js/filters/formatDuration.js
+++ front/app/js/filters/formatDuration.js
@@ -5,13 +5,13 @@ angular.module('upont').filter('formatDuration', function() {
                 if (duration % 3600 === 0)
                     return Math.floor(duration / 3600) + 'h';
                 else
-                    return Math.floor(duration / 3600) + 'h' + Math.floor((duration % 3600) / 60);
+                    return Math.floor(duration / 3600) + 'h ' + Math.floor((duration % 3600) / 60) + 'min';
             if (duration >= 60)
                 if (duration % 60 === 0)
-                    return Math.floor(duration / 60) + 'mn';
+                    return Math.floor(duration / 60) + 'min';
                 else
-                    return Math.floor(duration / 60) + 'mn' + Math.floor(duration % 60);
-            return duration + 's';
+                    return Math.floor(duration / 60) + 'min ' + Math.floor(duration % 60) + 's';
+            return duration + ' s';
         } else
             return null;
     };
