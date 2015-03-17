28bacd346d9762b0a1f96e4133b18c93250b6b16
Albéric <alberic.trancart@eleves.enpc.fr>
2015-03-17 22:51:12 +0100

    Fix bug d'alignement des colonnes sur Chrome

--- front/app/js/directives/upRow.js
+++ front/app/js/directives/upRow.js
@@ -6,13 +6,18 @@ angular.module('upont').directive('upRow', ['$window', function($window) {
                     element.addClass('left');
                 if(args.hasOwnProperty('wrap'))
                     element.addClass('wrap');
+                if(args.hasOwnProperty('center'))
+                    element.addClass('center');
                 if(args.hasOwnProperty('right'))
                     element.addClass('right');
                 if(args.hasOwnProperty('padded'))
                     element.addClass('padded');
+                if(args.hasOwnProperty('spaceAround'))
+                    element.addClass('space-around');
                 if(args.hasOwnProperty('spaceBetween'))
                     element.addClass('space-between');
                 element.addClass('up-row');
+                chromeFix();
             }
         }
     };
