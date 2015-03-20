2b0f31b311958d2a8a80700dfcd7076601fe913a
Albéric <alberic.trancart@eleves.enpc.fr>
2015-03-20 14:56:55 +0100

    Focus l'input de login

--- front/app/js/controllers/home.js
+++ front/app/js/controllers/home.js
@@ -1,5 +1,6 @@
 angular.module('upont')
     .controller('Disconnected_Ctrl', ['$scope', '$rootScope', '$state', 'StorageService', '$http', 'jwtHelper', '$resource', function($scope, $rootScope, $state, StorageService, $http, jwtHelper, $resource) {
+        $('#login-input').focus();
         $scope.login = function(pseudo, mdp) {
             if (pseudo.length && mdp.length)
                 $http
