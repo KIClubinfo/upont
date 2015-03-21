77e6ea61bbd03a680911b4bf8d875000b62f077c
Albéric <alberic.trancart@eleves.enpc.fr>
2015-03-21 01:18:38 +0100

    Encore une palanquée de debug

--- back/src/KI/UpontBundle/Controller/SearchController.php
+++ back/src/KI/UpontBundle/Controller/SearchController.php
@@ -73,10 +73,8 @@ class SearchController extends \KI\UpontBundle\Controller\Core\BaseController
                 $results = $this->searchRepo('Publications\Newsitem', $criteria);
                 break;
             case 'Club':
-                $results = $this->searchRepo('Users\\'.$category, $criteria);
-                break;
             case 'User':
-                $results = $this->searchUser($criteria);
+                $results = $this->searchRepo('Users\\'.$category, $criteria);
                 break;
             case 'Actor':
             case 'Genre':
@@ -120,34 +118,23 @@ class SearchController extends \KI\UpontBundle\Controller\Core\BaseController
     }
 
     // On fouille un repo à la recherche d'entités correspondantes au nom
-    private function searchRepo($repo, $criteria) {
-        $repo = $this->getDoctrine()->getManager()->getRepository('KIUpontBundle:'.$repo);
+    private function searchRepo($repoName, $criteria, $field = 'e.name') {
+        $repo = $this->getDoctrine()->getManager()->getRepository('KIUpontBundle:'.$repoName);
         $qb = $repo->createQueryBuilder('e');
         $searches = explode(' ', $criteria);
 
-        // On définit les paramètres de recherche
-        $cqb = array();
-        foreach ($searches as $key => $value) {
-            $cqb[] = $qb->expr()->like('e.name', $qb->expr()->literal('%'.$value.'%'));
+        // Si on a affaire au repo user on utilise un critère spécial
+        if ($repoName == 'Users\User') {
+            $field = $qb->expr()->concat('e.firstName',
+                      $qb->expr()->concat($qb->expr()->literal(' '),
+                       $qb->expr()->concat('e.lastName',
+                        $qb->expr()->concat($qb->expr()->literal(' '), 'COALESCE(e.nickname, \'\')'))));
         }
-        $qb->andWhere(call_user_func_array(array($qb->expr(), 'orx'), $cqb));
-        $results = $qb->setMaxResults(10)
-                      ->getQuery()
-                      ->getResult();
-
-        return $this->format($results, $criteria);
-    }
-
-    private function searchUser($criteria) {
-        $repo = $this->getDoctrine()->getManager()->getRepository('KIUpontBundle:Users\User');
-        $qb = $repo->createQueryBuilder('e');
-        $searches = explode(' ', $criteria);
 
         // On définit les paramètres de recherche
-        $concat = $qb->expr()->concat('e.firstName', $qb->expr()->concat($qb->expr()->literal(' '), 'e.lastName'));
         $cqb = array();
         foreach ($searches as $key => $value) {
-            $cqb[] = $qb->expr()->like($concat, $qb->expr()->literal('%'.$value.'%'));
+            $cqb[] = $qb->expr()->like($field, $qb->expr()->literal('%'.$value.'%'));
         }
         $qb->andWhere(call_user_func_array(array($qb->expr(), 'orx'), $cqb));
         $results = $qb->setMaxResults(10)
