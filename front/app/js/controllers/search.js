7ae5d007b07466ba0d89ef3440cb4549411e7f3a
Corentin de Boisset <corentin.de-boisset@eleves.enpc.fr>
2015-03-25 12:17:39 +0100

    edition recherche et textareas

--- front/app/js/controllers/search.js
+++ front/app/js/controllers/search.js
@@ -4,49 +4,11 @@ angular.module('upont')
         $scope.searchResults = {
             users: [],
             post: [],
-            channel: []
+            channel: [],
+            downloads: []
         };
 
-        // $scope.toggleCategories = function() {
-        //     $scope.showCategories = !$scope.showCategories;
-        // };
-
-        // $scope.changeCategory = function(category, searchValue) {
-        //     $rootScope.searchCategory = category;
-        //     $scope.doSearch(searchValue);
-        // };
-
-        // $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
-        //     // Changement de la catégorie de recherche
-        //     switch (toState.name) {
-        //         case 'root.ponthub.liste':
-        //         case 'root.ponthub.simple':
-        //             $rootScope.searchCategory = 'Ponthub';
-        //             break;
-        //         default:
-        //             $rootScope.searchCategory = 'Assos';
-        //     }
-
-        //     // On réinitialise la barre de recherche
-        //     // $scope.showCategories = false;
-        //     $scope.searchResults = [];
-        //     $('.search').val('');
-        // });
-
         $scope.doSearch = function(string) {
-            // var category = 'User';
-
-            // switch ($rootScope.searchCategory) {
-            //     case 'Ponthub':
-            //         category = 'Ponthub';
-            //         break;
-            //     case 'Publications':
-            //         category = 'Post';
-            //         break;
-            //     case 'Assos':
-            //         category = 'Club';
-            //         break;
-            // }
 
             if (string.length > 2) {
                 $http.post(apiPrefix + 'search', {search: 'User/' + string}).success(function(data){
@@ -58,11 +20,15 @@ angular.module('upont')
                 $http.post(apiPrefix + 'search', {search: 'Club/' + string}).success(function(data){
                     $scope.searchResults.channels = data;
                 });
+                $http.post(apiPrefix + 'search', {search: 'Ponthub/' + string}).success(function(data){
+                    $scope.searchResults.downloads = data;
+                });
             } else {
                 $scope.searchResults = {
                     users: [],
                     posts: [],
-                    channels: []
+                    channels: [],
+                    downloads: []
                 };
             }
         };
@@ -72,33 +38,42 @@ angular.module('upont')
 
             switch (result.type) {
                 case 'Movie':
-                    $state.go('root.ponthub.simple', {category: 'films', slug: slug});
+                    return 'root.ponthub.simple({category: "films", slug: slug})';
                     break;
-                case 'Serie':
-                    $state.go('root.ponthub.simple', {category: 'series', slug: slug});
-                    break;
-                case 'Episode':
-                    $state.go('root.ponthub.simple', {category: 'series', slug: slug});
-                    break;
-                case 'Album':
-                    $state.go('root.ponthub.simple', {category: 'musiques', slug: slug});
-                    break;
-                case 'Music':
-                    $state.go('root.ponthub.simple', {category: 'musiques', slug: slug});
-                    break;
-                case 'Game':
-                    $state.go('root.ponthub.simple', {category: 'jeux', slug: slug});
-                    break;
-                case 'Software':
-                    $state.go('root.ponthub.simple', {category: 'logiciels', slug: slug});
-                    break;
-                case 'Other':
-                    $state.go('root.ponthub.simple', {category: 'autres', slug: slug});
-                    break;
-                case 'Club':
-                    $state.go('root.channels.simple.publications', {slug: slug});
-                    break;
+                // case 'Serie':
+                //     $state.go('root.ponthub.simple', {category: 'series', slug: slug});
+                //     break;
+                // case 'Episode':
+                //     $state.go('root.ponthub.simple', {category: 'series', slug: slug});
+                //     break;
+                // case 'Album':
+                //     $state.go('root.ponthub.simple', {category: 'musiques', slug: slug});
+                //     break;
+                // case 'Music':
+                //     $state.go('root.ponthub.simple', {category: 'musiques', slug: slug});
+                //     break;
+                // case 'Game':
+                //     $state.go('root.ponthub.simple', {category: 'jeux', slug: slug});
+                //     break;
+                // case 'Software':
+                //     $state.go('root.ponthub.simple', {category: 'logiciels', slug: slug});
+                //     break;
+                // case 'Other':
+                //     $state.go('root.ponthub.simple', {category: 'autres', slug: slug});
+                //     break;
+                // case 'Club':
+                //     $state.go('root.channels.simple.publications', {slug: slug});
+                //     break;
             }
-            $scope.searchResults = [];
         };
+
+        $scope.resetSearch = function(){
+            $scope.searchValue = '';
+            $scope.searchResults = {
+                    users: [],
+                    posts: [],
+                    channels: [],
+                    downloads: []
+            };
+        }
     }]);
