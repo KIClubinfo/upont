06bb94de9a82131934ee061a13f55527d8a7aa0c
taquet-c <alberic.trancart@eleves.enpc.fr>
2015-03-25 18:10:23 +0100

    Overflow des textes

--- front/app/js/directives/upPubliText.js
+++ front/app/js/directives/upPubliText.js
@@ -5,31 +5,18 @@ angular.module('upont').directive('upPubliText', ['$filter', '$sce', function($f
             string: '='
         },
         link: function(scope, element, attrs){
-            //Dans un premier temps, on raccourcit par rapport au nombre de lignes
-            var string;
+            //Dans un premier temps, on cherche à savoir si le texte est trop gros
             var split = scope.string.split(/\r\n|\r|\n/);
-
-            if (split.length > 5 || scope.string.length > 550) {
-                scope.opened = false;
-                string = '';
-                for (var i = 0; i < 4; i++)
-                    string += split[i] + '<br>';
-                // En cas de publication vraiment très longue, on raccourcit
-                if (string.length > 550)
-                    scope.content = $sce.trustAsHtml($filter('stripTags')((string.substring(0, 350) + '... ').replace(/<br>\.{3}$/, '<br>'), allowedTags));
-                else
-                    scope.content = $sce.trustAsHtml($filter('stripTags')(string, allowedTags));
-            } else {
-                scope.opened = true;
-                scope.content = $sce.trustAsHtml($filter('stripTags')(scope.string, allowedTags));
-            }
+            scope.overflow = (split.length > 5 || scope.string.length > 550);
+            scope.content = $sce.trustAsHtml($filter('stripTags')(scope.string, allowedTags));
         },
         controller: ["$scope", function($scope) {
             $scope.open = function() {
-                $scope.opened = true;
+                $scope.overflow = false;
                 $scope.content = $sce.trustAsHtml($filter('stripTags')($scope.string, allowedTags));
             };
         }],
-        template: '<span class="up-ticket-texte"><span ng-bind-html="content"></span><span ng-if="!opened" class="up-link" ng-click="open()">Afficher la suite</span></span>',
+        template: '<div class="up-ticket-texte" ng-class="{\'up-overflow\': overflow}" ng-bind-html="content"></div>' +
+        '<span ng-if="overflow" class="up-link" ng-click="open()">Afficher la suite</span>',
     };
 }]);
