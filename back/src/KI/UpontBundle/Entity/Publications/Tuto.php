e0cc620457d28eb64bdd5d3232a5096aa715a6b8
Cécile <cecile.taquet-gasperini@eleves.enpc.fr>
2015-05-06 11:00:16 +0200

    Entité Tutoriel

---
+++ back/src/KI/UpontBundle/Entity/Publications/Tuto.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace KI\UpontBundle\Entity\Publications;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as JMS;
+use Symfony\Component\Validator\Constraints as Assert;
+use KI\UpontBundle\Entity\Core\Likeable;
+
+/**
+ * @ORM\Entity
+ * @JMS\ExclusionPolicy("all")
+ */
+class Tuto extends Likeable
+{
+    /**
+     * Corps du texte
+     * @ORM\Column(name="text", type="text")
+     * @JMS\Expose
+     * @Assert\Type("string")
+     * @Assert\NotBlank()
+     */
+    protected $text;
+
+    /**
+     * Date (timestamp)
+     * @ORM\Column(name="date", type="integer")
+     * @JMS\Expose
+     * @Assert\Type("integer")
+     */
+    protected $date;
+
+    /**
+     * Catégorie
+     * @ORM\Column(name="category", type="string")
+     * @JMS\Expose
+     * @Assert\Type("string")
+     * @Assert\NotBlank()
+     */
+    protected $category;
+
+
+
+
+
+    //===== GENERATED AUTOMATICALLY =====//
+
+    /**
+     * Set date
+     *
+     * @param integer $date
+     * @return Newsitem
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return integer
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     * @return Tuto
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set category
+     *
+     * @param string $category
+     * @return Tuto
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return string
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+}
---
+++ back/src/KI/UpontBundle/Tests/Controller/Publications/TutosControllerTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace KI\UpontBundle\Tests\Controller\Publications;
+
+use KI\UpontBundle\Tests\WebTestCase;
+
+class TutosControllerTest extends WebTestCase
+{
+    // On crée une ressource sur laquelle seront effectués les tests.
+    // Ne pas oublier de supprimer à la fin avec le test DELETE.
+    public function testPost()
+    {
+        $this->client->request(
+            'POST',
+            '/tutos',
+            array(
+                'name' => 'Proxy',
+                'text' => 'Pour régler le proxy faut aller dans "Réglages Proxy"',
+                'date' => 12345,
+                'category' => 'Réseau',
+                )
+            );
+        $response = $this->client->getResponse();
+        $this->assertJsonResponse($response, 201);
+        // On vérifie que le lieu du nouvel objet a été indiqué
+        $this->assertTrue($response->headers->has('Location'), $response->headers);
+    }
+
+    public function testGet()
+    {
+        $this->client->request('GET', '/tutos');
+        $response = $this->client->getResponse();
+        $this->assertJsonResponse($response, 200);
+
+        $this->client->request('GET', '/tutos/proxy');
+        $response = $this->client->getResponse();
+        $this->assertJsonResponse($response, 200);
+
+        $this->client->request('GET', '/tutos/sjoajsiohaysahais-asbsksaba7');
+        $response = $this->client->getResponse();
+        $this->assertJsonResponse($response, 404);
+    }
+
+    public function testPatch()
+    {
+        $this->client->request('PATCH', '/tutos/proxy', array('date' => 23456));
+        $response = $this->client->getResponse();
+        $this->assertJsonResponse($response, 204);
+
+        $this->client->request('PATCH', '/tutos/proxy', array('text' => ''));
+        $response = $this->client->getResponse();
+        $this->assertJsonResponse($response, 400);
+
+        $this->client->request('PATCH', '/tutos/sjoajslj', array('username' => 'miam', 'email' => '123@mail.fr'));
+        $response = $this->client->getResponse();
+        $this->assertJsonResponse($response, 404);
+    }
+
+    public function testDelete()
+    {
+        $this->client->request('DELETE', '/tutos/proxy');
+        $response = $this->client->getResponse();
+        $this->assertJsonResponse($response, 204);
+
+        $this->client->request('DELETE', '/tutos/proxy');
+        $response = $this->client->getResponse();
+        $this->assertJsonResponse($response, 404);
+    }
+}
