01f79b875b1e042b936c0dc59752e869fdfb0977
Adrian <adrian.lemaigre@eleves.enpc.fr>
2015-05-27 20:50:12 +0200

    Interface admin élèves

--- front/app/js/controllers/users/admin/students.js
+++ front/app/js/controllers/users/admin/students.js
@@ -1,5 +1,53 @@
 angular.module('upont')
-    .controller('Admin_Students_Ctrl', ['$scope', function($scope) {
+    .controller('Admin_Students_Ctrl', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
+        $scope.user = {
+            first_name: '',
+            last_name: '',
+            username: '',
+            email: ''
+        };
+
+        $scope.post = function(user, password, confirm) {
+            var params ={
+                firstName: user.first_name,
+                lastName: user.last_name,
+                username: user.username,
+                email: user.email
+            };
+
+            if (!user.first_name) {
+                alertify.error('Le prénom n\'a pas été renseigé');
+                return;
+            }
+
+            if (!user.last_name) {
+                alertify.error('Le nom n\'a pas été renseigé');
+                return;
+            }
+
+            if (!user.username) {
+                alertify.error('L\'identifiant DSI n\'a pas été renseigé');
+                return;
+            }
+
+            if (!user.email) {
+                alertify.error('L\'email n\'a pas été renseigé');
+                return;
+            }
+
+            if (password !== null && password !== '') {
+                if (password != confirm) {
+                    alertify.error('Les deux mots de passe ne sont pas identiques');
+                    return;
+                } else {
+                    params.plainPassword = {first: password, second: confirm};
+                }
+            }
+
+            $http.post($rootScope.url + 'users', params).success(function(){
+                alertify.success('Utilisateur créé');
+            });
+        };
 
     }])
     .config(['$stateProvider', function($stateProvider) {
