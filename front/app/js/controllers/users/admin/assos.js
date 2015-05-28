14f9d3f385c8baca123d817fc5b86e6cdb5f7cc3
Adrian <adrian.lemaigre@eleves.enpc.fr>
2015-05-28 10:31:40 +0200

    Page admin assos

--- front/app/js/controllers/users/admin/assos.js
+++ front/app/js/controllers/users/admin/assos.js
@@ -1,5 +1,45 @@
 angular.module('upont')
-    .controller('Admin_Assos_Ctrl', ['$scope', function($scope) {
+    .controller('Admin_Assos_Ctrl', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
+        $scope.club = {
+            fullname: '',
+            name: '',
+            administration: 'Non',
+            assos: 'Club'
+        };
+
+        $scope.post = function(club) {
+            var params ={
+                fullName: club.fullname,
+                name: club.name,
+                active: 1
+            };
+
+            if (!club.fullname) {
+                alertify.error('Le nom complet n\'a pas été renseigé');
+                return;
+            }
+
+            if (!club.name) {
+                alertify.error('Le nom court n\'a pas été renseigé');
+                return;
+            }
+
+            if (club.administration == 'Oui') {
+                params.administration = true;
+            } else {
+                params.administration = false;
+            }
+
+            if (club.assos == 'Assos') {
+                params.assos = true;
+            } else {
+                params.assos = false;
+            }
+
+            $http.post($rootScope.url + 'clubs', params).success(function(){
+                alertify.success('Assos créée');
+            });
+        };
 
     }])
     .config(['$stateProvider', function($stateProvider) {
