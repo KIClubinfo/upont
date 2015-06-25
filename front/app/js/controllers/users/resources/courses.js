e3668dfda47f7295fcd255c874f3720fb0be8e19
Albéric <alberic.trancart@eleves.enpc.fr>
2015-06-25 11:40:39 +0200

    Bugfixes et ajout de la structure de base des pages de cours

--- front/app/js/controllers/users/resources/courses.js
+++ front/app/js/controllers/users/resources/courses.js
@@ -1,6 +1,12 @@
 angular.module('upont')
-    .controller('Courses_Ctrl', ['$scope', function($scope) {
-
+    .controller('Courses_List_Ctrl', ['$scope', 'courses', function($scope, courses) {
+        $scope.courses = courses;
+    }])
+    .controller('Courses_Simple_Ctrl', ['$scope', 'course', 'exercices', function($scope, course, exercices) {
+        $scope.course = course;
+        $scope.exercices = exercices;
+        $scope.predicate = 'exercice.date';
+        $scope.reverse = false;
     }])
     .config(['$stateProvider', function($stateProvider) {
         $stateProvider
@@ -16,10 +22,45 @@ angular.module('upont')
             .state('root.users.resources.courses', {
                 url: '/cours',
                 abstract: true,
-                templateUrl: 'views/users/resources/courses.html',
+                template: '<div ui-view></div>',
                 data: {
                     title: 'Cours - uPont',
                     top: true
                 },
+            })
+            .state('root.users.resources.courses.list', {
+                url: '',
+                templateUrl: 'views/users/resources/courses.html',
+                data: {
+                    title: 'Liste des cours - uPont',
+                    top: true
+                },
+                controller: 'Courses_List_Ctrl',
+                resolve: {
+                    courses: ['$resource', function($resource) {
+                        return $resource(apiPrefix + 'courses').query().$promise;
+                    }]
+                },
+            })
+            .state('root.users.resources.courses.simple', {
+                url: '/:slug',
+                templateUrl: 'views/users/resources/course.html',
+                data: {
+                    title: 'Cours - uPont',
+                    top: true
+                },
+                controller: 'Courses_Simple_Ctrl',
+                resolve: {
+                    course: ['$resource', '$stateParams', function($resource, $stateParams) {
+                        return $resource(apiPrefix + 'courses/:slug').get({
+                            slug: $stateParams.slug
+                        }).$promise;
+                    }],
+                    exercices: ['$resource', '$stateParams', function($resource, $stateParams) {
+                        return $resource(apiPrefix + 'courses/:slug/exercices').query({
+                            slug: $stateParams.slug
+                        }).$promise;
+                    }]
+                }
             });
     }]);
---
+++ front/app/js/filters/courseHour.js
@@ -0,0 +1,6 @@
+angular.module('upont').filter('courseHour', function() {
+    return function(date) {
+        date = moment.unix(date);
+        return ucfirst(date.format('dddd') + ' à ' + date.format('HH:mm'));
+    };
+});
