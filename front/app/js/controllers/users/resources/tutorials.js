9894761df92f0b0813ee68958038369e62b8ed9f
Albéric <alberic.trancart@eleves.enpc.fr>
2015-07-12 11:23:16 +0200

    Ajout sections moderation et administration

--- front/app/js/controllers/users/resources/tutorials.js
+++ front/app/js/controllers/users/resources/tutorials.js
@@ -12,5 +12,22 @@ angular.module('upont')
                     title: 'Tutoriels - uPont',
                     top: true
                 },
-            });
+            })
+            .state('root.users.resources.administration', {
+                url: '/administration',
+                templateUrl: 'views/users/resources/administration.html',
+                data: {
+                    title: 'Infos Administration - uPont',
+                    top: true
+                },
+            })
+            .state('root.users.resources.moderation', {
+                url: '/moderation',
+                templateUrl: 'views/users/resources/moderation.html',
+                data: {
+                    title: 'Règles de modération - uPont',
+                    top: true
+                },
+            })
+        ;
     }]);
