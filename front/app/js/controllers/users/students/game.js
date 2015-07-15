bed61ef5fb87e75a590d3acb71c039589036086f
Adrian <adrian.lemaigre@eleves.enpc.fr>
2015-07-15 21:49:03 +0200

    Thumbnails

--- front/app/js/controllers/users/students/game.js
+++ front/app/js/controllers/users/students/game.js
@@ -75,13 +75,13 @@ angular.module('upont')
 
 				$scope.answer = $scope.gameData.list_users[$scope.position].answer;
 				$scope.name = $scope.gameData.list_users[$scope.position][$scope.answer][0];
-				$scope.picture = $scope.gameData.list_users[$scope.position][$scope.answer][1];
+				$scope.picture = '/api/' + $scope.gameData.list_users[$scope.position][$scope.answer][1].replace(/images/, 'thumbnails');
 				$scope.name1 = $scope.gameData.list_users[$scope.position][0][0];
 				$scope.name2 = $scope.gameData.list_users[$scope.position][1][0];
 				$scope.name3 = $scope.gameData.list_users[$scope.position][2][0];
-				$scope.picture1 = $scope.gameData.list_users[$scope.position][0][1];
-				$scope.picture2 = $scope.gameData.list_users[$scope.position][1][1];
-				$scope.picture3 = $scope.gameData.list_users[$scope.position][2][1];
+				$scope.picture1 = '/api/' + $scope.gameData.list_users[$scope.position][0][1].replace(/images/, 'thumbnails');
+				$scope.picture2 = '/api/' + $scope.gameData.list_users[$scope.position][1][1].replace(/images/, 'thumbnails');
+				$scope.picture3 = '/api/' + $scope.gameData.list_users[$scope.position][2][1].replace(/images/, 'thumbnails');
 
 				if (hardcore) {
 					$scope.trait = $scope.gameData.list_users[$scope.position].trait;
@@ -120,13 +120,13 @@ angular.module('upont')
 				$scope.change = $scope.position < ($scope.gameData.list_users.length)/2;
 				$scope.answer = $scope.gameData.list_users[$scope.position].answer;
 				$scope.name = $scope.gameData.list_users[$scope.position][$scope.answer][0];
-				$scope.picture = $scope.gameData.list_users[$scope.position][$scope.answer][1];
+				$scope.picture = '/api/' + $scope.gameData.list_users[$scope.position][$scope.answer][1].replace(/images/, 'thumbnails');
 				$scope.name1 = $scope.gameData.list_users[$scope.position][0][0];
 				$scope.name2 = $scope.gameData.list_users[$scope.position][1][0];
 				$scope.name3 = $scope.gameData.list_users[$scope.position][2][0];
-				$scope.picture1 = $scope.gameData.list_users[$scope.position][0][1];
-				$scope.picture2 = $scope.gameData.list_users[$scope.position][1][1];
-				$scope.picture3 = $scope.gameData.list_users[$scope.position][2][1];
+				$scope.picture1 = '/api/' + $scope.gameData.list_users[$scope.position][0][1].replace(/images/, 'thumbnails');
+				$scope.picture2 = '/api/' + $scope.gameData.list_users[$scope.position][1][1].replace(/images/, 'thumbnails');
+				$scope.picture3 = '/api/' + $scope.gameData.list_users[$scope.position][2][1].replace(/images/, 'thumbnails');
 
 				if ($scope.hardcore) {
 					$scope.trait = $scope.gameData.list_users[$scope.position].trait;
