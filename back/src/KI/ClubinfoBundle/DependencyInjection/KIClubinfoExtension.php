63446a45f25dfb2f28b7a2c232f075945b1f16da
Albéric <alberic.trancart@eleves.enpc.fr>
2015-08-02 23:04:04 +0200

    Promi, juré, cette fois-ci le ClubinfoBundle est 100% bon

--- back/src/KI/ClubinfoBundle/DependencyInjection/KIClubinfoExtension.php
+++ back/src/KI/ClubinfoBundle/DependencyInjection/KIClubinfoExtension.php
@@ -23,6 +23,7 @@ class KIClubinfoExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('listeners.yml');
         $loader->load('services.yml');
     }
 }
