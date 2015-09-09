00b651ef103dea68f162a975e9d96256025f8fd0
Adrian <adrian.lemaigre@eleves.enpc.fr>
2015-09-09 09:36:49 +0200

    Chrono affiche les ms

--- front/app/js/controllers/users/students/game.js
+++ front/app/js/controllers/users/students/game.js
@@ -168,4 +168,18 @@ angular.module('upont')
                     }],
                 },
             });
-    }]);
+    }])
+    .filter('numberFixedLen', function () {
+    	return function (n, l) {
+    		var num = parseInt(n,10);
+    		var len = parseInt(l,10);
+    		if (isNaN(num) || isNaN(len)) {
+    		    return n;
+    		}
+    		num = '' + num;
+    		while (num.length < len) {
+    			num = '0' + num;
+    		}
+    		return num;
+    	};
+    });
