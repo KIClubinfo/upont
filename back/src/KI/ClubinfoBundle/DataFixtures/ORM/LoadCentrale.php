6635926b730c0184c5eccfedd1500f65eb8435ab
Adrien <adrien.lina@eleves.enpc.fr>
2015-09-27 23:17:03 +0200

    chgmt route plus O2O/O2O

--- back/src/KI/ClubinfoBundle/DataFixtures/ORM/LoadCentrale.php
+++ back/src/KI/ClubinfoBundle/DataFixtures/ORM/LoadCentrale.php
@@ -11,13 +11,14 @@ class LoadCentraleFixture extends AbstractFixture implements OrderedFixtureInter
 {
     public function load(ObjectManager $manager)
     {
-        $fix = new Centrale();
-        $fix->setProduct('Clés USB');
-        $fix->setDescription('C\'est trotro cool, on va vous acheter pleins de clés USB pas cher du tout!');
-        $fix->setStartDate(1414242424);
-        $fix->setEndDate(1414242424);
-        $fix->setStatus('En cours');
-        $manager->persist($fix);
+        $centrale = new Centrale();
+        $centrale->setName('Clés USB');
+        $centrale->setDescription('C\'est trotro cool, on va vous acheter pleins de clés USB pas cher du tout!');
+        $centrale->setStartDate(1414242424);
+        $centrale->setEndDate(1414242424);
+        $centrale->setStatus('En cours');
+        $this->addReference('centrale-cles-usb', $user);
+        $manager->persist($centrale);
 
     }
 
