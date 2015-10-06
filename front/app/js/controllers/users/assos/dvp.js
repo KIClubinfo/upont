18793b92920c47a6307134dfae0b7cf604be88f9
Adrian <adrian.lemaigre@eleves.enpc.fr>
2015-10-06 19:19:51 +0200

    Interface pour commander en étant connecté

--- front/app/js/controllers/users/assos/dvp.js
+++ front/app/js/controllers/users/assos/dvp.js
@@ -1,7 +1,44 @@
 angular.module('upont')
-    .controller('DVP_Ctrl', ['$scope', '$rootScope', '$http', 'members', 'baskets', 'Paginate', function($scope, $rootScope, $http, members, baskets, Paginate) {
+    .controller('DVP_Ctrl', ['$scope', '$rootScope', '$http', 'baskets', 'Paginate', function($scope, $rootScope, $http, baskets, Paginate) {
         $scope.baskets = baskets;
-        //$scope.date = new date();
+        $scope.ordering = false;
+        $scope.thursdays = [];
+        $scope.months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
+        $scope.basketOrders = [[],[],[],[]];
+
+        for (i=0;i<4;i++) {
+            for (j=0;j<baskets.length;j++) {
+                $scope.basketOrders[i][j] = false;
+            }
+        }
+
+
+        $scope.order = function() {
+            $scope.ordering = true;
+            var d = new Date();
+            var diff;
+            
+            //We compute the next four thursdays.
+            if (d.getDay() > 1 || d.getHours() > 12) {
+                diff = 4 - d.getDay() + 7;
+                for (i=0;i<4;i++) {
+                    $scope.thursdays[i] = new Date();
+                    $scope.thursdays[i].setDate(d.getDate() + diff + i*7);
+                }
+            }
+            else {
+                diff = 4 - d.getDay();
+                for (i=0;i<4;i++) {
+                    $scope.thursdays[i] = new Date();
+                    $scope.thursdays[i].setDate(d.getDate() + diff + i*7);
+                }
+            }
+
+        };
+
+        $scope.post = function() {
+            alertify.success('C\'est parti !');
+        };
 
     }])
     .config(['$stateProvider', function($stateProvider) {
@@ -15,9 +52,6 @@ angular.module('upont')
                     top: true
                 },
                 resolve: {
-                    members: ['$resource', function($resource) {
-                        return $resource(apiPrefix + 'clubs/dvp/users').query().$promise;
-                    }],
                     baskets: ['$resource', function($resource) {
                         return $resource(apiPrefix + 'baskets').query().$promise;
                     }]
