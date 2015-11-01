0ac542af847dd7b21b6021b28934ecaeff1b4b97
Adrian <adrian.lemaigre@eleves.enpc.fr>
2015-11-01 13:34:14 +0100

    Poster et patcher ses propres entrées

--- front/app/js/controllers/users/students/pontlyvalent.js
+++ front/app/js/controllers/users/students/pontlyvalent.js
@@ -1,29 +1,77 @@
 angular.module('upont')
-	.controller('Students_Pontlyvalent_Ctrl', ['$scope', '$rootScope', '$resource', '$http', function($scope, $rootScope, $resource, $http) {
-		$scope.searchResult = [];
-		$scope.search = '';
+    .controller('Students_Pontlyvalent_Ctrl', ['$scope', '$rootScope', '$resource', '$http', '$q', function($scope, $rootScope, $resource, $http, $q) {
+        $scope.searchResultsPost = [];
+        $scope.searchPost = '';
+        $scope.searchResultsPatch = [];
+        $scope.searchPatch = '';
 
-        $scope.searchUser = function(string) {
+        $scope.searchUserPost = function(string) {
             if (string === '') {
-                $scope.searchResults = [];
+                $scope.searchResultsPost = [];
             } else {
                 $http.post(apiPrefix + 'search', {search: 'User/' + string}).success(function(data){
-                    $scope.searchResults = data.users;
+                    $scope.searchResultsPost = data.users;
                 });
             }
         };
 
+        $scope.searchUserPatch = function(string) {
+            if (string === '') {
+                $scope.searchResultsPatch = [];
+            } else {
+                $http.post(apiPrefix + 'search', {search: 'User/' + string}).success(function(data){
+                    $scope.searchResultsPatch = data.users;
+                });
+            }
+        };
 
-	}])
-	.config(['$stateProvider', function($stateProvider) {
-		$stateProvider
-			.state('root.users.students.pontlyvalent', {
-				url: '/pontlyvalent',
-				templateUrl: 'controllers/users/students/pontlyvalent.html',
-				controller: 'Students_Pontlyvalent_Ctrl',
-				data: {
-					title: 'Pontlyvalent - uPont',
-					top: true
-				},
-			});
-	}]);
+        $scope.loadComments = function() {
+            if ($rootScope.hasClub('bde')) {
+                  Paginate.first($scope.allComments).then(function(data){
+                    $scope.allComments = data;
+                });
+            }
+        };
+
+        $scope.addComment = function(slug, name) {
+            alertify.prompt('Entrée pour ' + name + ' :', function(e, text) {
+                if (e) {
+                    $http.post(apiPrefix + 'users/' + slug + '/pontlyvalent', {text: text}).success(function() {
+                        alertify.success('Entrée enregistrée');
+                    }).error(function() {
+                        alertify.error('Tu as déjà posté pour cette personne !');
+                    });
+                }
+            });
+        };
+
+        $scope.modifyComment = function(slug, name) {
+            $http.get(apiPrefix + 'users/' + slug + '/pontlyvalent').success(function(data) {
+                if (data.length === 0){
+                    alertify.error('Tu n\'as jamais posté pour cette personne !');
+                    return;
+                }
+
+                alertify.prompt('Modifier l\'entrée pour ' + name + ' :', function(e, text) {
+                    if (e) {
+                        $http.patch(apiPrefix + 'users/' + slug + '/pontlyvalent', {text: text}).success(function() {
+                            alertify.success('Entrée enregistrée');
+                        });
+                    }
+                }, data[0].text);
+            });
+        };
+
+    }])
+    .config(['$stateProvider', function($stateProvider) {
+        $stateProvider
+            .state('root.users.students.pontlyvalent', {
+                url: '/pontlyvalent',
+                templateUrl: 'controllers/users/students/pontlyvalent.html',
+                controller: 'Students_Pontlyvalent_Ctrl',
+                data: {
+                    title: 'Pontlyvalent - uPont',
+                    top: true
+                },
+            });
+    }]);
