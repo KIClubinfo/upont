aa61b9e94476ef38dd17d4f91094c27b42811e8a
Adrian <adrian.lemaigre@eleves.enpc.fr>
2015-11-04 21:56:08 +0100

    Remplace les tab par des espaces

--- front/app/js/controllers/users/students/pontlyvalent.js
+++ front/app/js/controllers/users/students/pontlyvalent.js
@@ -50,41 +50,41 @@ angular.module('upont')
         };
 
         $scope.addComment = function(slug, name) {
-        	//On get l'user pour voir s'il est dans la bonne promo
-        	$http.get(apiPrefix + 'users/' + slug).success(function(data) {
-        		if (data.promo != promo) {
-        			alertify.error('Tu ne peux poster que pour des 017 !');
-        			return;
-        		}
+            //On get l'user pour voir s'il est dans la bonne promo
+            $http.get(apiPrefix + 'users/' + slug).success(function(data) {
+                if (data.promo != promo) {
+                    alertify.error('Tu ne peux poster que pour des 017 !');
+                    return;
+                }
 
-        		//On regarde s'il y a déjà une entrée pour cet user de la part de l'utilisateur connecté
-        		$http.get(apiPrefix + 'users/' + slug + '/pontlyvalent').success(function(data) {
-        			if (data.length === 0) {
-        				//Si oui, on post
-        				alertify.prompt('Entrée pour ' + name + ' :', function(e, text) {
-        					if (e) {
-	        					$http.post(apiPrefix + 'users/' + slug + '/pontlyvalent', {text: text}).success(function() {
-			                        alertify.success('Entrée enregistrée');
-			                        $scope.reload();
-			                    });
-        					}
-        				});
-        			} else {
-        				//Sinon on patch
-        				alertify.prompt('Modifier l\'entrée pour ' + name + ' :', function(e, text) {
-		                    if (e) {
-		                        $http.patch(apiPrefix + 'users/' + slug + '/pontlyvalent', {text: text}).success(function() {
-		                            alertify.success('Entrée enregistrée');
-		                            $scope.reload();
-		                        });
-		                    }
-		                }, data[0].text);
-        			}
-        		});
-        	});
+                //On regarde s'il y a déjà une entrée pour cet user de la part de l'utilisateur connecté
+                $http.get(apiPrefix + 'users/' + slug + '/pontlyvalent').success(function(data) {
+                    if (data.length === 0) {
+                        //Si oui, on post
+                        alertify.prompt('Entrée pour ' + name + ' :', function(e, text) {
+                            if (e) {
+                                $http.post(apiPrefix + 'users/' + slug + '/pontlyvalent', {text: text}).success(function() {
+                                    alertify.success('Entrée enregistrée');
+                                    $scope.reload();
+                                });
+                            }
+                        });
+                    } else {
+                        //Sinon on patch
+                        alertify.prompt('Modifier l\'entrée pour ' + name + ' :', function(e, text) {
+                            if (e) {
+                                $http.patch(apiPrefix + 'users/' + slug + '/pontlyvalent', {text: text}).success(function() {
+                                    alertify.success('Entrée enregistrée');
+                                    $scope.reload();
+                                });
+                            }
+                        }, data[0].text);
+                    }
+                });
+            });
         };
 
-        
+
     }])
     .config(['$stateProvider', function($stateProvider) {
         $stateProvider
