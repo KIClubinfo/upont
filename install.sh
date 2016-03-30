0da62769027d4cce8d5f3f9a8f3e921caec7faed
Louis Trezzini <louis.trezzini@eleves.enpc.fr>
2016-03-30 12:33:38 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# [AT'016] Script d'installation total de YouPont
+# [LT'018] Prérequis uPont sans vagrant
 
 echo -e "\e[1m\e[34mBienvenue sur le script d'installation de uPont"
 echo -e "Ce script est destiné aux distributions Ubuntu-like. \e[31mUn compte GitHub est nécessaire.\e[0m"
 read -p "Adresse mail du compte GitHub : " mail
 read -p "Prénom Nom : " name
-read -p "Nom du compte utilisateur Linux : " user
 read -p "Adresse proxy (au format http://user:password@etuproxy.enpc.fr:3128) : " proxy
 
 ### INSTALL ###
 echo -e "\e[1m\e[34mInstallation des dépendances...\e[0m"
-sudo apt-get install -y lamp-server^ php5-curl php5-intl php5-gd php5-imap phpmyadmin curl phpunit git nodejs npm ant gource libav-tools xvfb phantomjs
+sudo apt-get update
+sudo apt-get install -y curl expect git htop iotop lynx make nano netcat traceroute sl tree vim unzip zip mysql-server python-mysqldb php5-cli php5-fpm php5-curl php5-gd php5-imap php5-intl php5-mcrypt php5-mysql nginx apt-transport-https
 
 echo -e "\e[1m\e[34mConfiguration du proxy...\e[0m"
 if [ -z "$proxy" ]; then
@@ -19,8 +19,6 @@ else
     npm config set proxy $proxy
     npm config set https-proxy $proxy
 fi
-sudo ln -s /usr/bin/nodejs /usr/bin/node
-sudo npm install -g gulp bower phonegap cordova
 
 echo -e "\e[1m\e[34mConfiguration de git...\e[0m"
 sudo chown -R $user:www-data /var/www
@@ -31,50 +29,30 @@ git config --global http.postBuffer 524288000
 git config --global push.default simple
 git config --global credential.helper 'cache --timeout=86400'
 
-echo -e "\e[1m\e[34mClonage du repo...\e[0m"
-git clone https://github.com/KIClubinfo/upont.git
-cd upont
-
-echo -e "\e[1m\e[34mActivation de phpmyadmin et réglages Apache...\e[0m"
-sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-enabled/phpmyadmin.conf
-sudo cp utils/upont.enpc.fr.conf /etc/apache2/sites-available/upont.enpc.fr.conf
-sudo a2ensite upont.enpc.fr
-sudo a2dissite 000-default
-sudo cp utils/php.ini /etc/php5/apache2/php.ini
-sudo a2enmod rewrite
-sudo service apache2 reload
-
-echo -e "\e[1m\e[34mInstallation du front...\e[0m"
-cd front
-sudo npm install
-bower install
-gulp build
-
-echo -e "\e[1m\e[34mInstallation de l'appli mobile...\e[0m"
-cd ../mobile
-sudo npm install
-bower install
-gulp build
-cordova plugin add org.apache.cordova.device org.apache.cordova.splashscreen https://github.com/phonegap-build/PushPlugin.git
-
-echo -e "\e[1m\e[34mInstallation du back...\e[0m"
-cd ../back
-
 echo -e "\e[1m\e[34mCréation d'une base de données...\e[0m"
 echo "CREATE DATABASE upont" | mysql -u root -p
 
-echo -e "\e[1m\e[34mInstallation de composer...\e[0m"
-curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
-composer install
+sudo cp utils/install/www.conf /etc/php5/fpm/pool.d/www.conf
+sudo mkdir /etc/php5/conf.d
+sudo cp utils/install/global.ini /etc/php5/conf.d/global.ini
 
-echo -e "\e[1m\e[34mMise à jour de la BDD...\e[0m"
-php app/console doctrine:schema:update --force
-php app/console doctrine:fixtures:load
+sudo curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin
+sudo mv /usr/local/bin/composer.phar /usr/local/bin/composer
+chmod -R 0777 ~/.composer/cache
 
-echo -e "\e[1m\e[34mVidage du cache...\e[0m"
-php app/console cache:clear
-sudo chmod 777 -R app/cache && sudo chmod 777 -R app/logs && sudo chmod 777 -R web/uploads
+sudo cp utils/install/php5-fpm.conf /etc/nginx/conf.d/php5-fpm.conf
+sudo service restart php5-fpm
 
-echo -e "\e[1m\e[34mTest final à effectuer manuellement pour vérifier que tout va bien...\e[0m"
-phpunit -c app/
+sudo cp utils/install/dev-upont.enpc.fr.conf /etc/nginx/sites-available/dev-upont.enpc.fr.conf
+sudo ln -s /etc/nginx/sites-available/dev-upont.enpc.fr.conf /etc/nginx/sites-enabled/dev-upont.enpc.fr.conf
+sudo service nginx restart
+
+wget -qO - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
+sudo cp utils/install/nodesource.list /etc/apt/sources.list.d/nodesource.list
+sudo cp utils/install/deb_nodesource_com_node.pref /etc/apt/preferences.d/deb_nodesource_com_node.pref
+
+sudo apt-get update
+sudo apt-get install nodejs
+sudo npm install -g npm bower gulp
+
+echo "127.0.0.1 dev-upont.enpc.fr" | sudo tee -a /etc/hosts
