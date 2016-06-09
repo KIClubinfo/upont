4062ff97ba22c527fe3b365b7c72ad6c449dbc00
Louis Trezzini <louis.trezzini@eleves.enpc.fr>
2016-06-09 16:24:43 +0200

    Changement des dates admissibles

--- front/app/js/controllers/admissibles/index.js
+++ front/app/js/controllers/admissibles/index.js
@@ -9,25 +9,25 @@ angular.module('upont').controller('Admissibles_Ctrl', ['$scope', '$location', '
             'https://upont.enpc.fr/plaquette.pdf'
         ];
 
-        var serie1 = {
-            openDate: moment("2016-06-09 20:00 +0200", "YYYY-MM-DD HH:mm Z"),
-            closeDate: moment("2016-06-12 23:00 +0200", "YYYY-MM-DD HH:mm Z"),
-            serie: 1,
+        $scope.shotgun = {
+            openDate: moment("2016-06-15 20:00 +0200", "YYYY-MM-DD HH:mm Z"),
+            serie1CloseDate: moment("2016-06-16 20:00 +0200", "YYYY-MM-DD HH:mm Z"),
+            closeDate: moment("2016-06-20 23:00 +0200", "YYYY-MM-DD HH:mm Z"),
         };
-
-        $scope.shotgun = serie1;
         $scope.shotgun.openDateString = $scope.shotgun.openDate.format('LLLL');
+        $scope.shotgun.serie1CloseDateString = $scope.shotgun.serie1CloseDate.format('LLLL');
         $scope.shotgun.closeDateString = $scope.shotgun.closeDate.format('LLLL');
 
         var tick = function () {
-            var now = moment();
             $scope.shotgun.fromNow = $scope.shotgun.openDate.fromNow();
-            $scope.shotgunOpen = $scope.shotgun.openDate.isBefore();
+            $scope.shotgunOpen = $scope.shotgun.openDate.isBefore() && $scope.shotgun.closeDate.isAfter();
+            $scope.serie1Open = $scope.shotgun.openDate.isBefore() && $scope.shotgun.serie1CloseDate.isAfter();
             $timeout(tick, 1000);
         };
         $timeout(tick, 1000);
 
-        $scope.shotgunOpen = $scope.shotgun.openDate.isBefore();
+        $scope.shotgunOpen = $scope.shotgun.openDate.isBefore() && $scope.shotgun.closeDate.isAfter();
+        $scope.serie1Open = $scope.shotgun.openDate.isBefore() && $scope.shotgun.serie1CloseDate.isAfter();
 
         $scope.submit = function(data) {
             if (data.lastName === undefined || data.firstName === undefined || data.scei === undefined || data.contact === undefined || data.serie === undefined || data.room === undefined || data.lastName === '' || data.firstName === '' || data.scei === '' || data.contact === '' || data.serie === '' || data.room === '') {
