ca5cc10beffb461a63303787ea364a6fca9a7e8e
Louis Trezzini <louis.trezzini@eleves.enpc.fr>
2016-06-16 22:11:20 +0200

    On retire le cas particulier pour la serie 1

--- front/app/js/controllers/admissibles/index.js
+++ front/app/js/controllers/admissibles/index.js
@@ -11,23 +11,19 @@ angular.module('upont').controller('Admissibles_Ctrl', ['$scope', '$location', '
 
         $scope.shotgun = {
             openDate: moment("2016-06-15 20:00 +0200", "YYYY-MM-DD HH:mm Z"),
-            serie1CloseDate: moment("2016-06-16 20:00 +0200", "YYYY-MM-DD HH:mm Z"),
             closeDate: moment("2016-06-20 23:00 +0200", "YYYY-MM-DD HH:mm Z"),
         };
         $scope.shotgun.openDateString = $scope.shotgun.openDate.format('LLLL');
-        $scope.shotgun.serie1CloseDateString = $scope.shotgun.serie1CloseDate.format('LLLL');
         $scope.shotgun.closeDateString = $scope.shotgun.closeDate.format('LLLL');
 
         var tick = function () {
             $scope.shotgun.fromNow = $scope.shotgun.openDate.fromNow();
             $scope.shotgunOpen = $scope.shotgun.openDate.isBefore() && $scope.shotgun.closeDate.isAfter();
-            $scope.serie1Open = $scope.shotgun.openDate.isBefore() && $scope.shotgun.serie1CloseDate.isAfter();
             $timeout(tick, 1000);
         };
         $timeout(tick, 1000);
 
         $scope.shotgunOpen = $scope.shotgun.openDate.isBefore() && $scope.shotgun.closeDate.isAfter();
-        $scope.serie1Open = $scope.shotgun.openDate.isBefore() && $scope.shotgun.serie1CloseDate.isAfter();
 
         $scope.submit = function(data) {
             if (data.lastName === undefined || data.firstName === undefined || data.scei === undefined || data.contact === undefined || data.serie === undefined || data.room === undefined || data.lastName === '' || data.firstName === '' || data.scei === '' || data.contact === '' || data.serie === '' || data.room === '') {
