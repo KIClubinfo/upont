4ee287b17032027e84c929a581b75067d8b64be3
Louis Trezzini <louis.trezzini@eleves.enpc.fr>
2016-06-23 15:53:28 +0200

    Serie performance

---
+++ back/src/KI/PonthubBundle/Listener/SerieListener.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace KI\PonthubBundle\Listener;
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use KI\PonthubBundle\Entity\Serie;
+
+class SerieListener
+{
+    public function postLoad(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        if ($entity instanceof Serie) {
+            $entity->setDownloads(
+                $args->getEntityManager()->createQuery("SELECT COUNT(pfu.id) FROM
+                    KIPonthubBundle:Episode episode, 
+                    KIPonthubBundle:PonthubFileUser pfu
+                    WHERE pfu.file = episode
+                    AND episode.serie = :serie
+                    ")
+                ->setParameter('serie', $entity)
+                ->getSingleScalarResult()
+            );
+        }
+    }
+}
