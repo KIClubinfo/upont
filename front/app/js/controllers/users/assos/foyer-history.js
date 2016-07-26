ca306b36f2411b87935ac2e53c66335ee73a4268
Charles <charles.auguste@ymail.com>
2016-07-26 22:27:35 +0200

    Utilisation de Paginate pour afficher l'historique.

--- front/app/js/controllers/users/assos/foyer-history.js
+++ front/app/js/controllers/users/assos/foyer-history.js
@@ -1,8 +1,15 @@
 angular.module('upont')
    .controller('Foyer_History_Ctrl', ['$scope', '$rootScope', '$http', 'historique', 'Paginate', function($scope, $rootScope, $http, historique, Paginate) {
     $scope.historique = historique;
-   }])
-   .config(['$stateProvider', function($stateProvider) {
+
+    $scope.next = function() {
+        Paginate.next($scope.historique).then(function(data){
+            $scope.historique = data;
+        });
+    };
+
+    }])
+    .config(['$stateProvider', function($stateProvider) {
        $stateProvider
            .state('root.users.assos.foyer-history', {
                url: '/foyer/historique',
@@ -13,8 +20,10 @@ angular.module('upont')
                    top: true
                },
                resolve: {
-                   historique: ['$resource', '$rootScope', function($resource, $rootScope) {
-                       return $resource(apiPrefix + 'users/:slug/transactions').query( {slug: $rootScope.username}).$promise;
+                   //historique: ['$resource', '$rootScope', function($resource, $rootScope) {
+                   //    return $resource(apiPrefix + 'users/:slug/transactions').query( {slug: $rootScope.username}).$promise;
+                   historique: ['Paginate', '$rootScope', function(Paginate, $rootScope) {
+                        return Paginate.get('users/' + $rootScope.username +'/transactions?sort=-date', 8);
                    }]
                }
            });
