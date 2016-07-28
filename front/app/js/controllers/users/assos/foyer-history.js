9fe3ce0e7b29e96784c687c1ec36a09bd0171419
Louis Trezzini <louis.trezzini@eleves.enpc.fr>
2016-07-28 14:27:01 +0200

    Formatting + remove commented code

--- front/app/js/controllers/users/assos/foyer-history.js
+++ front/app/js/controllers/users/assos/foyer-history.js
@@ -1,30 +1,28 @@
 angular.module('upont')
-   .controller('Foyer_History_Ctrl', ['$scope', '$rootScope', '$http', 'historique', 'Paginate', function($scope, $rootScope, $http, historique, Paginate) {
-    $scope.historique = historique;
+    .controller('Foyer_History_Ctrl', ['$scope', '$rootScope', '$http', 'historique', 'Paginate', function ($scope, $rootScope, $http, historique, Paginate) {
+        $scope.historique = historique;
 
-    $scope.next = function() {
-        Paginate.next($scope.historique).then(function(data){
-            $scope.historique = data;
-        });
-    };
+        $scope.next = function () {
+            Paginate.next($scope.historique).then(function (data) {
+                $scope.historique = data;
+            });
+        };
 
     }])
-    .config(['$stateProvider', function($stateProvider) {
-       $stateProvider
-           .state('root.users.assos.foyer-history', {
-               url: '/foyer/historique',
-               templateUrl: 'controllers/users/assos/foyer-history.html',
-               controller: 'Foyer_History_Ctrl',
-               data: {
-                   title: 'Historique foyer - uPont',
-                   top: true
-               },
-               resolve: {
-                   //historique: ['$resource', '$rootScope', function($resource, $rootScope) {
-                   //    return $resource(apiPrefix + 'users/:slug/transactions').query( {slug: $rootScope.username}).$promise;
-                   historique: ['Paginate', '$rootScope', function(Paginate, $rootScope) {
-                        return Paginate.get('users/' + $rootScope.username +'/transactions?sort=-date', 8);
-                   }]
-               }
-           });
-   }]);
+    .config(['$stateProvider', function ($stateProvider) {
+        $stateProvider
+            .state('root.users.assos.foyer-history', {
+                url: '/foyer/historique',
+                templateUrl: 'controllers/users/assos/foyer-history.html',
+                controller: 'Foyer_History_Ctrl',
+                data: {
+                    title: 'Historique foyer - uPont',
+                    top: true
+                },
+                resolve: {
+                    historique: ['Paginate', '$rootScope', function (Paginate, $rootScope) {
+                        return Paginate.get('users/' + $rootScope.username + '/transactions?sort=-date', 8);
+                    }]
+                }
+            });
+    }]);
