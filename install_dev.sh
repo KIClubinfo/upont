aebccdac4aa6991e0ab9d6fbf7cb81d23e9e2e90
Corentin de Boisset <corentin.deboisset@gmail.com>
2016-09-28 21:13:02 +0200

    update installation script

--- install_dev.sh
+++ install_dev.sh
@@ -16,16 +16,18 @@ gulp build-templates
 gulp copy-fonts
 
 cd ../back
-composer self-update
+
+HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
+sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
+sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
+
 composer install
-sudo chmod 777 -R var/cache && sudo chmod 777 -R var/logs && sudo chmod 777 -R web/uploads
-bin/console cache:clear
-sudo rm -rf var/cache/*
-sudo rm -rf var/logs/*
-bin/console doctrine:mi:mi -n
+php bin/console assets:install
+php bin/console doctrine:migrations:migrate --no-interaction
+php bin/console cache:warmup
+
 rm -rf web/uploads/exercices/*
 rm -rf web/uploads/files/*
 rm -rf web/uploads/images/*
 rm -rf web/uploads/tmp/*
 echo "Y" | bin/console doctrine:fixtures:load
-sudo chmod 777 -R var/cache && sudo chmod 777 -R var/logs && sudo chmod 777 -R web/uploads
