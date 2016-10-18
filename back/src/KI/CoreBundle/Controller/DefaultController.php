8c61d4185bc57fd43fd051f81879ac0d756be188
Louis Trezzini <louis.trezzini@eleves.enpc.fr>
2016-10-18 15:48:04 +0200

    Send client IP to front

--- back/src/KI/CoreBundle/Controller/DefaultController.php
+++ back/src/KI/CoreBundle/Controller/DefaultController.php
@@ -243,11 +243,12 @@ class DefaultController extends BaseController
     public function configAction(Request $request)
     {
         return $this->json([
-            "studentNetwork" => IpUtils::checkIp($request->getClientIp(), [
-                "172.24.0.0-172.24.0.255",
-                "172.24.20.0-172.24.60.255",
-                "172.24.100.0-172.24.200.255",
-                "195.221.194.14-195.221.194.14",
+            'ip' => $request->getClientIp(),
+            'studentNetwork' => IpUtils::checkIp($request->getClientIp(), [
+                '172.24.0.0-172.24.0.255',
+                '172.24.20.0-172.24.60.255',
+                '172.24.100.0-172.24.200.255',
+                '195.221.194.14-195.221.194.14',
             ])
         ]);
     }
