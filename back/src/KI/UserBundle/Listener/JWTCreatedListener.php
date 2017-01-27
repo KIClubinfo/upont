d371842b756a2065cfdc06534fc3724a01d909b6
Louis Trezzini <louis.trezzini@eleves.enpc.fr>
2017-01-27 23:21:03 +0100

    Lexik update

--- back/src/KI/UserBundle/Listener/JWTCreatedListener.php
+++ back/src/KI/UserBundle/Listener/JWTCreatedListener.php
@@ -18,13 +18,8 @@ class JWTCreatedListener
     {
         $request = $this->requestStack->getCurrentRequest();
 
-        // Le token expire une semaine plus tard à 2h du matin
-        $expiration = new \DateTime('+7 day');
-        $expiration->setTime(2, 0, 0);
-
         $payload = $event->getData();
         $payload['ip'] = $request->getClientIp();
-        $payload['exp'] = $expiration->getTimestamp();
 
         $event->setData($payload);
     }
