f85e98574651b9c5a9527f7a9a08a77d8783bf1a
Louis Trezzini <louis.trezzini@eleves.enpc.fr>
2017-01-27 17:10:13 +0100

    Improve config usage

--- back/src/KI/CoreBundle/Controller/CoreController.php
+++ back/src/KI/CoreBundle/Controller/CoreController.php
@@ -49,47 +49,6 @@ class CoreController extends Controller
     }
 
     /**
-     * Sert à checker si l'user actuel est membre du club au nom duquel il poste
-     * @param  string $club
-     * @return boolean
-     */
-    protected function isClubMember($club = null)
-    {
-        if ($this->is('ADMISSIBLE')) {
-            return false;
-        }
-
-        // On vérifie que la requete est valide.
-        // Si aucun club n'est précisé, c'est qu'on publie à son nom
-        // (par exemple message perso) donc ok
-        $request = $this->get('request_stack')->getCurrentRequest()->request;
-        if (!$request->has('authorClub') && $club === null) {
-            return $this->is('USER');
-        }
-
-        $repo = $this->manager->getRepository('KIUserBundle:Club');
-        $club = $repo->findOneBySlug($request->has('authorClub') ? $request->get('authorClub') : $club);
-
-        if (!$club) {
-            return false;
-        }
-
-        // On vérifie que l'utilisateur fait bien partie du club
-        return $this->get('ki_user.service.permission')->isClubMember($this->user, $club);
-    }
-
-    /**
-     * Sert à checker si l'user actuel est membre du club au nom duquel il poste
-     * @param  string $club
-     * @return boolean
-     */
-    protected function isFoyerMember()
-    {
-        return $this->isClubMember('foyer')
-            && $this->user->getPromo() == $this->container->getParameter('upont')['promos']['assos'];
-    }
-
-    /**
      * Génère une réponse au format JSON en parsant les propriétés avec le FOSRestBundle
      * @param  mixed $data    Le contenu à renvoyer
      * @param  int   $status    Le code d'erreur HTTP à renvoyer
@@ -133,5 +92,28 @@ class CoreController extends Controller
             default:
                 return $this->json($data['item'], $data['code']);
         }
+    }
+
+    /**
+     * Retourne une configuration de uPont
+     * @param  string  $path   La clé de configuration
+     * @return string
+     */
+    public function getConfig($path)
+    {
+        $config = $this->container->getParameter('upont');
+
+        if (!empty($path)) {
+            $keys = preg_split('/[\.]/', $path);
+            foreach ($keys as $key) {
+                if (isset($config[$key])) {
+                    $config = $config[$key];
+                } else {
+                    throw new \OutOfBoundsException;
+                }
+            }
+        }
+
+        return $config;
     }
 }
