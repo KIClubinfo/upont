8eba9233392b3e45948e95a318ff915269a5abae
Louis Trezzini <louis.trezzini@eleves.enpc.fr>
2018-02-11 11:16:27 +0100

    Add dummy migration

---
+++ back/src/Migrations/Version20180210213458.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180210213458 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Event CHANGE startDate startDate DATETIME NOT NULL, CHANGE endDate endDate DATETIME NOT NULL, CHANGE shotgunDate shotgunDate DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Event CHANGE startDate startDate INT NOT NULL, CHANGE endDate endDate INT NOT NULL, CHANGE shotgunDate shotgunDate INT DEFAULT NULL');
+    }
+}
