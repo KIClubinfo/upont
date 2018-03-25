a4107befbbe2ae7257be57a2321c0008ad739823
Louis Trezzini <louis.trezzini@illuin.tech>
2018-03-25 15:27:05 +0200

    Super migration

--- back/src/Migrations/Version20180325120346.php
+++ back/src/Migrations/Version20180325120346.php
@@ -15,8 +15,33 @@ class Version20180325120346 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE Event CHANGE startDate startDate DATETIME NOT NULL COMMENT \'(DC2Type:carbondatetime)\', CHANGE endDate endDate DATETIME NOT NULL COMMENT \'(DC2Type:carbondatetime)\', CHANGE shotgunDate shotgunDate DATETIME DEFAULT NULL COMMENT \'(DC2Type:carbondatetime)\'');
+        // Event
+
+        $this->addSql('ALTER TABLE Event ADD newDate DATETIME');
+
+        $this->addSql('UPDATE Event SET newDate = FROM_UNIXTIME(startDate)');
+        $this->addSql('ALTER TABLE Event CHANGE startDate startDate DATETIME NOT NULL COMMENT \'(DC2Type:carbondatetime)\'');
+        $this->addSql('UPDATE Event SET startDate = newDate');
+
+        $this->addSql('UPDATE Event SET newDate = FROM_UNIXTIME(endDate)');
+        $this->addSql('ALTER TABLE Event CHANGE endDate endDate DATETIME NOT NULL COMMENT \'(DC2Type:carbondatetime)\'');
+        $this->addSql('UPDATE Event SET endDate = newDate');
+
+        $this->addSql('UPDATE Event SET newDate = FROM_UNIXTIME(shotgunDate)');
+        $this->addSql('ALTER TABLE Event CHANGE shotgunDate shotgunDate DATETIME DEFAULT NULL COMMENT \'(DC2Type:carbondatetime)\'');
+        $this->addSql('UPDATE Event SET shotgunDate = newDate');
+
+        $this->addSql('ALTER TABLE Event DROP COLUMN newDate');
+
+        // EventUser
+
+        $this->addSql('ALTER TABLE EventUser ADD newDate DATETIME');
+
+        $this->addSql('UPDATE EventUser SET newDate = FROM_UNIXTIME(shotgunDate)');
         $this->addSql('ALTER TABLE EventUser CHANGE shotgunDate shotgunDate DATETIME NOT NULL COMMENT \'(DC2Type:carbondatetime)\'');
+        $this->addSql('UPDATE EventUser SET shotgunDate = newDate');
+
+        $this->addSql('ALTER TABLE EventUser DROP COLUMN newDate');
     }
 
     public function down(Schema $schema)
@@ -24,7 +49,32 @@ class Version20180325120346 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE Event CHANGE startDate startDate INT NOT NULL, CHANGE endDate endDate INT NOT NULL, CHANGE shotgunDate shotgunDate INT DEFAULT NULL');
+        // Event
+
+        $this->addSql('ALTER TABLE Event ADD newDate INT DEFAULT NULL');
+
+        $this->addSql('UPDATE Event SET newDate = UNIX_TIMESTAMP(startDate)');
+        $this->addSql('ALTER TABLE Event CHANGE startDate startDate INT NOT NULL');
+        $this->addSql('UPDATE Event SET startDate = newDate');
+
+        $this->addSql('UPDATE Event SET newDate = UNIX_TIMESTAMP(endDate)');
+        $this->addSql('ALTER TABLE Event CHANGE endDate endDate INT NOT NULL');
+        $this->addSql('UPDATE Event SET endDate = newDate');
+
+        $this->addSql('UPDATE Event SET newDate = UNIX_TIMESTAMP(shotgunDate)');
+        $this->addSql('ALTER TABLE Event CHANGE shotgunDate shotgunDate INT DEFAULT NULL');
+        $this->addSql('UPDATE Event SET shotgunDate = newDate');
+
+        $this->addSql('ALTER TABLE Event DROP COLUMN newDate');
+
+        // EventUser
+
+        $this->addSql('ALTER TABLE EventUser ADD newDate INT DEFAULT NULL');
+
+        $this->addSql('UPDATE EventUser SET newDate = UNIX_TIMESTAMP(shotgunDate)');
         $this->addSql('ALTER TABLE EventUser CHANGE shotgunDate shotgunDate INT NOT NULL');
+        $this->addSql('UPDATE EventUser SET shotgunDate = newDate');
+
+        $this->addSql('ALTER TABLE EventUser DROP COLUMN newDate');
     }
 }
