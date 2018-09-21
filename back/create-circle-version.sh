2762cf98c5c7889d53eaad69ecc3e7621525b7c3
Louis Trezzini <louis.trezzini@ponts.org>
2018-09-21 19:46:32 +0200

    Try fixing version update bash script

--- back/create-circle-version.sh
+++ back/create-circle-version.sh
@@ -2,7 +2,7 @@
 set -eux
 
 VERSION_REGEX="^v([0-9]+)\.([0-9]+)\.([0-9]{1}.*)$"
-if [[ ${CIRCLE_TAG:-} =~ ${VERSION_REGEX} ]]; then
+if [[ ${VERSION_TAG:-} =~ ${VERSION_REGEX} ]]; then
     VERSION_MAJOR="${BASH_REMATCH[1]}"
     VERSION_MINOR="${BASH_REMATCH[2]}"
     VERSION_PATCH="${BASH_REMATCH[3]}"
@@ -11,7 +11,6 @@ else
     VERSION_MINOR=0
     VERSION_PATCH=0
 fi
-VERSION_HASH=${CIRCLE_SHA1}
 VERSION_DATE=$(date +%s)
 
 cat > ./config/version.yaml <<EOL
