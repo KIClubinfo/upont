09aeb3a41d960eef481024220aab5309b0199f33
Youval <youval@vanlaer.fr>
2018-09-26 14:10:20 +0200

    minor fixes

--- back/src/KI/UserBundle/Command/PhotoUpdateCommand.php
+++ back/src/KI/UserBundle/Command/PhotoUpdateCommand.php
@@ -150,7 +150,7 @@ class PhotoUpdateCommand extends ContainerAwareCommand
                 $currentIndex += 1;
                 $matchingRow = 0;
             }
-            if ($k < strlen($longName)) {
+            if ($currentIndex < strlen($longName)) {
                 //Parameters may be adapted
                 if ($i == 0 && $currentIndex == 0) {
                     $score += 10;
@@ -159,11 +159,11 @@ class PhotoUpdateCommand extends ContainerAwareCommand
                     $score += 10;
                 }
                 if ($lastIndex > -1) {
-                    $score += 5 * exp(-($k - $lastIndex) / 2);
+                    $score += 5 * exp(-($currentIndex - $lastIndex) / 2);
                 }
-                $lastIndex = $k;
+                $lastIndex = $currentIndex;
                 $matchingRow += 1;
-                $score += exp($matchingRow/2);
+                $score += exp($matchingRow / 2);
                 $currentIndex += 1;
             }
         }
@@ -175,11 +175,11 @@ class PhotoUpdateCommand extends ContainerAwareCommand
     // -met tout en minuscule
     private function cleanString(string $str)
     {
-	$unwanted_array = array(    'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+	$unwanted_array = ['Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
                             'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
                             'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
                             'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
-                            'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y' );
+                            'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y'];
         $str = strtr( $str, $unwanted_array );
         return strtolower($str);
     }
