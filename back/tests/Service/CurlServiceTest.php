6cbc27e7e1cc115be777efb17e19c6771da1e98d
Louis Trezzini <louis.trezzini@ponts.org>
2019-01-28 23:22:56 +0100

    fix tests

--- back/tests/Service/CurlServiceTest.php
+++ back/tests/Service/CurlServiceTest.php
@@ -2,13 +2,14 @@
 
 namespace Tests\App\Services;
 
+use App\Service\CurlService;
 use App\Tests\WebTestCase;
 
 class CurlServiceTest extends WebTestCase
 {
     public function testCurl()
     {
-        $curlService = static::$kernel->getContainer()->get('App\Service\CurlService');
+        $curlService = static::$kernel->getContainer()->get(CurlService::class);
         $response = $curlService->curl('https://www.google.fr');
         $this->assertInternalType('string', $response);
         $this->assertContains('<!doctype html>', $response);
--- back/tests/Service/ImageServiceTest.php
+++ back/tests/Service/ImageServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\App\Services;
 
+use App\Service\ImageService;
 use App\Tests\WebTestCase;
 
 class ImageServiceTest extends WebTestCase
@@ -15,7 +16,7 @@ class ImageServiceTest extends WebTestCase
         parent::setUp();
 
         $this->container = static::$kernel->getContainer();
-        $this->service = $this->container->get('App\Service\ImageService');
+        $this->service = $this->container->get(ImageService::class);
         $this->path = $this->container->getParameter('ki_core.images.directory');
     }
 
